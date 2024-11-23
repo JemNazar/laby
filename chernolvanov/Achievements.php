0bea833003c840924a22b1a199dd16d8f5852cf6
JemNazar <86036121+JemNazar@users.noreply.github.com>
2024-11-23 20:46:10 +0500

    Add files via upload

---
+++ chernolvanov/Achievements.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Проффесиональные достижения</title>
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <!-- Put this script tag to the <head> of your page -->
+<script type="text/javascript" src="//vk.com/js/api/openapi.js?152"></script>
+</head>
+<body>
+  <?php
+		require_once "blocks/headerL.php"
+	?>
+<div class="container">
+<h4 class="blue-text ">Проффесиональные достижения</h4>
+<center>
+	<div class="row">
+	<div class="col l4 s12">
+	<div class="card">
+	<img class="materialboxed" width="250" src="/images/dip/d1.jpg">
+	</div>
+	</div>
+	<div class="col l4 s12">
+	<div class="card">
+	<img class="materialboxed" width="250" src="/images/dip/d2.jpg">
+	</div>
+	</div>
+	<div class="col l4 s12">
+	<div class="card">
+	<img class="materialboxed" width="250" src="/images/dip/d3.jpg">
+	</div>
+	</div>
+	<div class="col l4 s12">
+	<div class="card">
+	<img class="materialboxed" width="250" src="/images/dip/d4.jpg">
+	</div>
+	</div>
+		<div class="col l4 s12">
+	<div class="card">
+	<img class="materialboxed" width="250" src="/images/dip/d6.jpg">
+	</div>
+	</div>
+	<div class="card col l12 s12">
+	<img class="materialboxed col l6 s12" width="250" src="/images/dip/d5-1.jpg">
+	<img class="materialboxed col l6 s12" width="250" src="/images/dip/d5-2.jpg">
+	</div>
+	
+	</div>
+	
+</center>
+</div>
+	</div> 
+  <?php
+		require_once "blocks/footer.php"
+	?>
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+  <script>$('.dropdown-button').dropdown({ hover: true}); </script>
+  
+  </body>
+</html>
